8181f18d700481377be34159d77b30bd9c32bd5c
Johan Acosta Rojas <andreyacosta181003@gmail.com>
2026-01-16 18:32:12 -0500

    chore: setup tailwind v4 and robots-txt integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+import robotTxt from "astro-robots-txt";
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss(), robotTxt()],
+  },
+});
