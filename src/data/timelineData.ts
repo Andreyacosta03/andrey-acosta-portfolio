7db871fd47da1e7eff3c54f933ec8b5764392fb5
Johan Acosta Rojas <andreyacosta181003@gmail.com>
2026-01-24 16:03:54 -0500

    feat: Add ProfessionalTrajectory.astro component to index.astro

---
+++ src/data/timelineData.ts
@@ -0,0 +1,32 @@
+const education = [
+  {
+    date: "2021 — 2024 (AGO)",
+    title: "Ingeniería de Software",
+    company: "Corp. Universitaria Minuto de Dios",
+    description: "En etapa de finalización / Grado próximo.",
+  },
+  {
+    date: "2020",
+    title: "Tecnólogo en Desarrollo de Software",
+    company: "Corp. Universitaria Minuto de Dios",
+  },
+];
+const experience = [
+  {
+    date: "2023 — Actualidad",
+    title: "Desarrollador de Software",
+    company: "Parquesoft (App Salud Mental)",
+    description:
+      "Lideré el desarrollo de una solución móvil para tratamiento terapéutico con agendamiento y pruebas automatizadas.",
+    tags: ["FLUTTER", "NODE.JS"],
+  },
+  {
+    date: "2023",
+    title: "Freelance Developer",
+    company: "App Educativa Ortografía",
+    description:
+      "Diseño y desarrollo de app gamificada con base de datos en tiempo real.",
+    tags: ["REACT NATIVE", "FIREBASE"],
+  },
+];
+export { education, experience };
